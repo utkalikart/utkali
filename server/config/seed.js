92b54663449e93a46dfb5421c1a8ca556206bdac
utkalikart <32205795+utkalikart@users.noreply.github.com>
2017-09-24 13:19:37 +0530

    Add files via upload

---
+++ server/config/seed.js
@@ -0,0 +1,18 @@
+/**
+ * Populate DB with sample data on server start
+ * to disable, edit config/environment/index.js, and set `seedDB: false`
+ */
+
+'use strict';
+
+//var Chart = require('../api/Chart/chart.model');
+/*var chartData = require('../data/chartData.json');*/
+//var chartData = {};
+
+//Chart.find(function(err,chartData) {
+//if(err) { return handleError(res, err); }
+  //chartData = chartData
+//})
+
+
+
