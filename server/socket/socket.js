b6f919a161fd69ce2b047e93464446ebd7591ef1
utkalikart <32205795+utkalikart@users.noreply.github.com>
2017-09-24 13:09:13 +0530

    Add files via upload

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -1,1 +1,19 @@
 
+
+module.exports = function(io) {
+  io.on('connection', function(socket) {
+    console.log('new connection - '+socket.id);
+
+    socket.on('user_id', function(data) {
+      console.log("user_id",data);
+      socket.user_id = data.user_id;
+      socket.randomUserId = data.randomUserId;
+      socket.join(socket.user_id+"-"+socket.randomUserId);
+    });
+
+    socket.on('disconnect',function(reason) {
+      console.log("disconnect - "+socket.id)
+      socket.leave(socket.user_id+"-"+socket.randomUserId);
+    })
+  });
+}
